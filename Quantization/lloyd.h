d193ac36f6a0cf57b2dc43751066d787b9e86f4d
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2020-10-18 21:25:37 +0900

    백업

--- Quantization/lloyd.h
+++ Quantization/lloyd.h
@@ -9,13 +9,18 @@ using namespace std;
 #define ISIZE 540*960
 #define L 256 // 2^8
 
-unsigned short ** m_ui16Comp; // input image
-unsigned short ** m_ui16Comp2; // reconstructed image
-unsigned char **m_ui8Comp; // quantized image
+unsigned short ** m_ui16Comp; // Input image
+unsigned short ** m_ui16Comp2; // Reconstructed image
+unsigned char **m_ui8Comp; // Quantized image
 
-int m_iSize[3] = { ISIZE, ISIZE / 4, ISIZE / 4 };
+int m_iSize[3] = { ISIZE, ISIZE / 4, ISIZE / 4 }; // YCbCr420 format
 
-void initialize();
+void initialize(); // This function uniformly divides the total range into the number of levels
+void calculateIntervals(); // Calculate the interval/range values D = (R[i] + R[i - 1]) / 2
+void getNewLevels(unsigned short **P); // Recompute the values of R using the probability values
+float getQuantizedValue_Y(unsigned short key); // This function gives the quantized value given the key(value)
+float getQuantizedValue_Cb(unsigned short key); // This function gives the quantized value given the key(value)
+float getQuantizedValue_Cr(unsigned short key); // This function gives the quantized value given the key(value)
 void printD();
 void printR();
 
