48a44ad7504fb32d096dfe8700185d2f729f4fdd
Tae Sung Park <46318721+developerTae@users.noreply.github.com>
2020-10-18 15:00:32 +0900

    YCbCr 채널 분리

--- Quantization/lloyd.h
+++ Quantization/lloyd.h
@@ -7,10 +7,11 @@
 using namespace std;
 
 #define ISIZE 540*960
+#define L 256 // 2^8
 
-unsigned short ** m_ui16Comp;
-unsigned char **m_ui8Comp;
-
+unsigned short ** m_ui16Comp; // input image
+unsigned short ** m_ui16Comp2; // reconstructed image
+unsigned char **m_ui8Comp; // quantized image
 
 int m_iSize[3] = { ISIZE, ISIZE / 4, ISIZE / 4 };
 
